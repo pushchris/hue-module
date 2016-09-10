fd65c94dfe8e7ce752c66a575c0185d4f84294b8
Richard Slater <github@richard-slater.co.uk>
2016-09-10 19:22:19 +0100

    Tidied up WhiteSpace.

--- example.js
+++ example.js
@@ -7,22 +7,22 @@ hue.discover(function (error, host) {
     }
 
     console.log("Discovered HUE at %s", host);
-    
+
     // Need to register for key first (not currently supported)
     hue.load({
         "host"  : host,
         "key"   : "APPLICATION_KEY"
-    }); 
+    });
 
     hue.lights(function(lights) {
-        
+
         for (i in lights) {
             if (lights.hasOwnProperty(i)) {
                 hue.change(lights[i].set({
-                    "on"    : true, 
+                    "on"    : true,
                     "rgb"   : [
-                        Math.random() * 256 >>> 0, 
-                        Math.random() * 256 >>> 0, 
+                        Math.random() * 256 >>> 0,
+                        Math.random() * 256 >>> 0,
                         Math.random() * 256 >>> 0
                     ]
                 }));
