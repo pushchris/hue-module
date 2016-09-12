2c2229b0e10138948d96147ae89102bc56e6759d
Richard Slater <github@richard-slater.co.uk>
2016-09-12 20:52:12 +0100

    Fix Lint Warning W033: Missing Semicolon

--- example.js
+++ example.js
@@ -4,52 +4,52 @@ var loadBridge = function(host) {
 
     hue.load({
         "host"  : host
-    }); 
-    
+    });
+
     hue.getUsername(function(err, result) {
         if (err) {
             console.log(err);
             return;
         }
-        
+
         turnOnLights(host, result.username);
     });
-}
+};
 
 var turnOnLights = function(host, username) {
-    
+
     hue.load({
         "host"  : host,
         "key"   : username
-    }); 
-    
+    });
+
     hue.lights(function(lights) {
-        
+
         for (i in lights) {
             if (lights.hasOwnProperty(i)) {
                 hue.change(lights[i].set({
-                    "on"    : true, 
+                    "on"    : true,
                     "rgb"   : [
-                        Math.random() * 256 >>> 0, 
-                        Math.random() * 256 >>> 0, 
+                        Math.random() * 256 >>> 0,
+                        Math.random() * 256 >>> 0,
                         Math.random() * 256 >>> 0
                     ]
                 }));
             }
         }
     });
-}
+};
 
 hue.nupnpDiscover(function(error, hosts) {
-    
+
     if (error) {
         console.error(error);
         return;
     }
-    
+
     for (var i in hosts) {
         if (hosts.hasOwnProperty(i)) {
-            
+
             loadBridge(hosts[i]["internalipaddress"]);
         }
     }
